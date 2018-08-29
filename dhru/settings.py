ed11f11292a30173214d9ad74fbbce1887177e38
vladimirkl1 <vladimirkl1@gmail.com>
2018-08-29 13:30:52 +0300

    Update settings.py

--- dhru/settings.py
+++ dhru/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4calyikngz=*_t=00yrvr$detgl9x4@pwl5+8fgu1t++w3%w*x'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['digital-human.ru', 'www.digital-human.ru']
+ALLOWED_HOSTS = ['', '']
 
 # Ensure SITE_ID is set sites app
 SITE_ID = 2
